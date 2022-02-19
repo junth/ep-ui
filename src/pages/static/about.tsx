94402ec92b42f7b934c097a8f473b6f9cabf80ae
Royal-lobster <srujangs8@gmail.com>
2022-02-19 13:06:32 +0530

    added about page hero

--- src/pages/static/about.tsx
+++ src/pages/static/about.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+import AboutHero from '@/components/About/AboutHero'
 
-const About = () => <main>about page</main>
+const About = () => (
+  <Box maxW="7xl" mx="auto" mt={4}>
+    <AboutHero />
+  </Box>
+)
 
 export default About
