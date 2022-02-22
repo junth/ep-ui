b2e7b4bcd1b2ddaf94735749355ffe437f2d2639
JustinCast <justincastillovalladares@gmail.com>
2022-02-22 15:24:12 -0600

    wip: trying to fix vercel deployment error

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Box, Container, Heading } from '@chakra-ui/react'
 import Connectors from '@/components/Layout/WalletDrawer/Connectors'
 
-const Login = () =>
-    <Container centerContent mt="8" mb="24">
-      <Box w="xl">
-        <Heading fontWeight="extrabold" fontSize={23}>
-          Connect your wallet
-        </Heading>
-        <Connectors />
-      </Box>
-    </Container>
+const Login = () => (
+  <Container centerContent mt="8" mb="24">
+    <Box w="xl">
+      <Heading fontWeight="extrabold" fontSize={23}>
+        Connect your wallet
+      </Heading>
+      <Connectors />
+    </Box>
+  </Container>
+)
 
 export default Login
