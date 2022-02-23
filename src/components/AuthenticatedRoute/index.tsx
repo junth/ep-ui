0d2352a80a30d747fc6bf056fc343d8e71e6209a
Drebakare <bakaredmlr@gmail.com>
2022-02-23 20:56:56 +0100

    fix: fixes eslint warning of useEffect dependencies in authenticated route

--- src/components/AuthenticatedRoute/index.tsx
+++ src/components/AuthenticatedRoute/index.tsx
@@ -3,14 +3,19 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-export const authenticatedRoute =<P extends object> (WrappedComponent: React.ComponentType<P>) => {
-  const AuthenticatedRoute  = (props: P) => {
+export const authenticatedRoute = <P extends object>(
+  WrappedComponent: React.ComponentType<P>,
+) => {
+  const AuthenticatedRoute = (props: P) => {
     const router = useRouter()
     const { user } = useSelector((state: RootState) => state.user)
     useEffect(() => {
-      if (!user) {
-        router.push('/login')
+      const verifyUserAuthentication = () => {
+        if (!user) {
+          router.push('/login')
+        }
       }
+      verifyUserAuthentication()
     }, [])
     if (user) {
       return <WrappedComponent {...props} />
@@ -19,4 +24,3 @@ export const authenticatedRoute =<P extends object> (WrappedComponent: React.Com
   }
   return AuthenticatedRoute
 }
-  
