7d49bf5ac79969104cbb7e11540a2b9be436da8f
Drebakare <bakaredmlr@gmail.com>
2022-02-23 18:32:52 +0100

    deltes console.log

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,4 +1,4 @@
-import {authenticatedRoute} from '@/components/AuthenticatedRoute'
+import { authenticatedRoute } from '@/components/AuthenticatedRoute'
 import React from 'react'
 
 const Profile = () => <div>Profile Page</div>
