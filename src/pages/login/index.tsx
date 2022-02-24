f1e1bb69fa11c0f4342033e1aa52a74f26cd74ea
Drebakare <bakaredmlr@gmail.com>
2022-02-24 15:47:01 +0100

    staging

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,25 +6,24 @@ import { RootState } from '@/store/store'
 import { useRouter } from 'next/router'
 
 const Login = () => {
-  const {user} = useSelector((state: RootState) => state.user)
+  const { user } = useSelector((state: RootState) => state.user)
   const router = useRouter()
-  useEffect(()=> {
-    if(user){
-        router.back()
+  useEffect(() => {
+    if (user) {
+      router.back()
     }
   }, [user])
 
-  return(
+  return (
     <Container centerContent mt="8" mb="24">
-    <Box w="xl">
-      <Heading fontWeight="extrabold" fontSize={23}>
-        Connect your wallet
-      </Heading>
-      <Connectors />
-    </Box>
-  </Container>
+      <Box w="xl">
+        <Heading fontWeight="extrabold" fontSize={23}>
+          Connect your wallet
+        </Heading>
+        <Connectors />
+      </Box>
+    </Container>
   )
 }
-
 
 export default Login
