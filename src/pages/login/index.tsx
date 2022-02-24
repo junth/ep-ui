b8e736619fa2929c5439359458cf4fa3d08ac0bd
Drebakare <bakaredmlr@gmail.com>
2022-02-24 03:45:37 +0100

    feat: implements redirection on login page on user authentication

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Container, Heading } from '@chakra-ui/react'
 import Connectors from '@/components/Layout/WalletDrawer/Connectors'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store/store'
+import { useRouter } from 'next/router'
 
-const Login = () => (
-  <Container centerContent mt="8" mb="24">
+const Login = () => {
+  const {user} = useSelector((state: RootState) => state.user)
+  const router = useRouter()
+  useEffect(()=> {
+    if(user){
+        router.back()
+    }
+  }, [user])
+
+  return(
+    <Container centerContent mt="8" mb="24">
     <Box w="xl">
       <Heading fontWeight="extrabold" fontSize={23}>
         Connect your wallet
@@ -11,6 +23,8 @@ const Login = () => (
       <Connectors />
     </Box>
   </Container>
-)
+  )
+}
+
 
 export default Login
