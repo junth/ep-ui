01b8db3dd6b7b95ff8b1b15ef5f43c1fb52f870f
Abraham <anubra266@gmail.com>
2022-02-24 20:03:06 +0100

    homepage notable drops and featured article

---
+++ src/data/FeaturesArticleData.ts
@@ -0,0 +1,15 @@
+type FeaturedArticle = {
+  image: string
+  avatarImage: string
+  username: string
+  title: string
+}
+
+export const featuredArticle: FeaturedArticle = {
+  image:
+    'https://lh3.googleusercontent.com/uBIIhl5iCZViJ133SnvHJg7aQdF2Qa-qJIoezA6sevODQXSx2igYea8jC_adwYtGbppFFdNVg8oVbwLsMBfO-SPKTJU7zjUGajRQ=s550',
+  avatarImage:
+    'https://lh3.googleusercontent.com/wgcZwtqicGBzmbt21J4cEMNg_64kFwwgHLB703Ma1_TgNpDZPiJzTuYgZBP_5NVqOVQuhEHW-0ft3uRROmeyPcSMKgSj-JFuSFy1=s80',
+  username: 'kesar.eth',
+  title: 'Framework Ventures',
+}
