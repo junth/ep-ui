6dd3138f26fd9106d4e8765d0dc5590361cc9d06
JustinCast <justincastillovalladares@gmail.com>
2022-02-28 11:39:35 -0600

    Update src/utils/getAccount.ts
    Co-authored-by: Abraham <anubra266@gmail.com>

--- src/utils/getAccount.ts
+++ src/utils/getAccount.ts
@@ -1,9 +1,6 @@
 import shortenAccount from './shortenAccount'
 
-export const getAccount = (account: any, shorten = false): string =>
-  // eslint-disable-next-line no-nested-ternary
-  account?.ens?.name
-    ? `${account.ens?.name}`
-    : shorten
-    ? shortenAccount(account?.address)
-    : account?.address
+export const getAccount = (account: any, shorten = false): string => {
+  if (account?.ens?.name) return account.ens?.name
+  return shorten ? shortenAccount(account?.address) : account?.address
+}
