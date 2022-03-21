50fa634d0a4e2a7d6b70560231cc54873dbd31cd
James Akpan <jamesakpan016@gmail.com>
2022-03-21 15:02:07 +0100

    Updating Browserstorage to remove expiry bug.  (#76)
    * Fixes issues#159 Updated localstorage to remove expiry bug
    
    * Fixes issues#159 Updated localstroage to remove expiry bug

--- src/utils/browserStorage.ts
+++ src/utils/browserStorage.ts
@@ -4,17 +4,24 @@ const storageKey = 'serializedState'
 const currentDate = new Date()
 const expiryTimeline = 86400
 
+const getLocalStorage = () => {
+  const setExpiry = JSON.parse(localStorage.getItem(storageKey) || '{}')
+  return setExpiry.expiry
+}
+
 export const loadState = () => {
   try {
-    const serializedInitialState = localStorage.getItem(storageKey)
+    const serializedInitialState = JSON.parse(
+      localStorage.getItem(storageKey) || '{}',
+    )
 
     if (!serializedInitialState) return undefined
 
-    const setExpiry = JSON.parse(localStorage.getItem(storageKey) || '{}')
-    if (currentDate.getTime() > setExpiry) {
+    if (currentDate.getTime() > serializedInitialState.expiry) {
       localStorage.removeItem(storageKey)
     }
-    return JSON.parse(serializedInitialState)
+    const { updatedState } = serializedInitialState
+    return updatedState
   } catch (e) {
     return undefined
   }
@@ -36,7 +43,10 @@ export function saveState(state: RootState) {
     }
     const preSerializedState = {
       updatedState,
-      expiry: currentDate.getTime() + expiryTimeline,
+      expiry:
+        currentDate.getTime() < getLocalStorage()
+          ? getLocalStorage()
+          : currentDate.getTime() + expiryTimeline,
     }
     const serializedState = JSON.stringify(preSerializedState)
     localStorage.setItem(storageKey, serializedState)
