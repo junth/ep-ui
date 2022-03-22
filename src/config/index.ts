5fbfe0fd240dfc771b6150e491e6bef6074b5343
César Rodríguez <kesarr@gmail.com>
2022-03-22 10:29:17 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,7 +17,7 @@ const config = {
   mumbaiTestnetChainId: 80001,
   ethereumMainnetChainId: 1,
   binanceSmartChainId: 56,
-  epApiBaseUrl: process.env.NEXT_PUBLIC_EP_API,
+  epApiBaseUrl: process.env.NEXT_PUBLIC_EP_API || 'https://api.dev.braindao.org/',
 }
 
 export default config
