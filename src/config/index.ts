e08ecebe2d164571b60f4f765a48e0b378cef2c1
César Rodríguez <kesarr@gmail.com>
2022-03-23 18:12:31 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ const config = {
     process.env.NEXT_PUBLIC_WIKI_CONTRACT_ADDRESS ||
     '0x9332ad5290cf8de41107712ef408eb0c47dcb057',
   blockExplorerUrl: String(process.env.NEXT_PUBLIC_BLOCK_EXPLORER_BASE_URL),
-  pinataBaseUrl: process.env.NEXT_PUBLIC_PINATA_GATEWAY_BASE_URL,
+  pinataBaseUrl: process.env.NEXT_PUBLIC_PINATA_GATEWAY_BASE_URL || 'https://ipfs.everipedia.org',
   infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
   graphqlUrl: 'https://api.dev.braindao.org/graphql',
   chainId: String(process.env.NEXT_PUBLIC_CHAIN_ID),
