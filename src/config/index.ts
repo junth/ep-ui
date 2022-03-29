b46116258601b4936648398a6dfd0dfab68a56a8
César Rodríguez <kesarr@gmail.com>
2022-03-29 12:43:36 +0200

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,7 +9,7 @@ const config = {
     'https://mumbai.polygonscan.com/',
   pinataBaseUrl:
     process.env.NEXT_PUBLIC_PINATA_GATEWAY_BASE_URL ||
-    'https://ipfs.everipedia.org',
+    'https://ipfs.everipedia.org/ipfs/',
   infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
   graphqlUrl: 'https://api.dev.braindao.org/graphql',
   chainId: process.env.NEXT_PUBLIC_CHAIN_ID || '80001',
