19c8179832ca27764303573e2cfdb20ae0b4b711
anubra266 <anubra266@gmail.com>
2022-04-21 11:15:17 +0100

    progress

--- src/components/Wiki/WikiPage/InsightComponents/Uniswap.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/Uniswap.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { SwapWidget, darkTheme, lightTheme } from '@uniswap/widgets'
 // import '@uniswap/widgets/dist/fonts.css'
 
 import { useColorModeValue } from '@chakra-ui/color-mode'
-import { provider } from '@/pages/_app'
+import detectEthereumProvider from '@metamask/detect-provider'
+import { ethers, providers } from 'ethers'
 
 // Infura endpoint
 const jsonRpcEndpoint = process.env.NEXT_PUBLIC_ENS_RPC
+const jsonRpcProvider = new providers.JsonRpcProvider(jsonRpcEndpoint)
+const defProvider = new ethers.providers.Web3Provider(jsonRpcProvider as any)
 
 export const Uniswap = () => {
   const theme = useColorModeValue(lightTheme, darkTheme)
-  const uniswapProvider = provider()
+
+  const [provider, setProvider] = useState<any>(defProvider)
+
+  useEffect(() => {
+    const setProv = async () => {
+      const ethProvider = await detectEthereumProvider()
+      console.log('ethProvider :>> ', ethProvider);
+      if (ethProvider) setProvider(ethProvider)
+    }
+    setProv()
+  }, [])
 
   return (
     <div>
       <SwapWidget
-        provider={uniswapProvider}
+        provider={provider}
         theme={theme}
         jsonRpcEndpoint={jsonRpcEndpoint}
       />
