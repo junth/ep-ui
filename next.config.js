63da4191f06dc0d18703e2c3ec555ce115f9df32
JustinCast <justincastillovalladares@gmail.com>
2022-04-23 14:17:01 -0600

    testing loader

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
   styledComponents: true,
   images: {
     loader: 'imgix',
-    path: '',
+    path: 'https://ep-test.imgix.net/',
     domains: [
       'picsum.photos',
       'everipedia.org',
