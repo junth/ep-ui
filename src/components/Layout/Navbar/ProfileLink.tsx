2a61e06ed09d457c212cda9c5fbbab8a71249f98
Drebakare <bakaredmlr@gmail.com>
2022-05-16 08:21:15 +0100

    reworks profile link navigation

--- src/components/Layout/Navbar/ProfileLink.tsx
+++ src/components/Layout/Navbar/ProfileLink.tsx
@@ -1,32 +1,30 @@
 import { Box, MenuItem, Link, Icon } from '@chakra-ui/react'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 import { RiAccountCircleFill } from 'react-icons/ri'
 import { useAccount } from 'wagmi'
 
 export const ProfileLink = () => {
   const { data } = useAccount()
+  const router = useRouter()
+  const [link, setLink] = useState(data?.address ? `/account/${data?.address}` : '/login')
+
+  useEffect(()=> {
+    setLink(data?.address ? `/account/${data?.address}` : '/login')
+  }, [data?.address])
+
   return (
-    <Link
-      order={-1}
-      href={data?.address ? `/account/${data?.address}` : '/login'}
-      _hover={{
-        textDecoration: 'none',
-        color: 'linkHoverColor',
-      }}
-      color="linkColor"
-    >
-      <MenuItem minH="48px" bg="subMenuBg">
-        <Icon
-          cursor="pointer"
-          fontSize="4xl"
-          fontWeight={600}
-          as={RiAccountCircleFill}
-          pr={3}
-        />
-        <Box fontSize="md" fontWeight={600} color="linkColor">
-          Profile
-        </Box>
-      </MenuItem>
-    </Link>
+    <MenuItem order={-1} onClick={()=>router.push(link)} minH="48px" bg="subMenuBg">
+      <Icon
+        cursor="pointer"
+        fontSize="4xl"
+        fontWeight={600}
+        as={RiAccountCircleFill}
+        pr={3}
+      />
+      <Box fontSize="md" fontWeight={600} color="linkColor">
+        Profile
+      </Box>
+    </MenuItem>
   )
 }
