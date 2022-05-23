c6c37bfcf76139622eb682139c11924ea5ccafb6
Bakare Damilare E <70170061+Emmanueldmlr@users.noreply.github.com>
2022-05-23 13:18:39 +0100

    Align avatar (#280)
    * fixes hero avatar alignment
    
    * fixes lint error
    
    * align avatar with the address
    
    * fixes lint error"
    
    * rework homepage avatar and address
    
    * fixes lint error

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -9,6 +9,7 @@ import {
   Text,
   VStack,
   Stack,
+  Link,
 } from '@chakra-ui/react'
 import { LinkButton } from '@/components/Elements'
 import { Wiki } from '@/types/Wiki'
@@ -19,9 +20,10 @@ import { useTranslation } from 'react-i18next'
 import { useENSData } from '@/hooks/useENSData'
 import CustomAvatar from 'boring-avatars'
 import { AvatarColorArray } from '@/data/AvatarData'
-import { getWikiSummary, WikiSummarySize } from '@/utils/getWikiSummary'
+import { getWikiSummary } from '@/utils/getWikiSummary'
 import { WikiImage } from '../WikiImage'
 
+const CARD_DETAILS_LENGTH = 50
 const HeroCard = ({ wiki }: HeroProps) => {
   const [avatar, username] = useENSData(wiki?.user?.id)
 
@@ -49,36 +51,39 @@ const HeroCard = ({ wiki }: HeroProps) => {
           overflow="hidden"
           roundedTop="lg"
         />
-        <Flex p="3" align="center" gap={4}>
-          <NextLink href={`/account/${wiki?.user?.id}`} passHref>
-            <Box>
-              {avatar ? (
-                <Avatar size="xs" src={avatar} />
-              ) : (
-                <CustomAvatar
-                  size="25"
-                  variant="pixel"
-                  name="Unnamed"
-                  colors={AvatarColorArray}
-                />
-              )}
-            </Box>
-          </NextLink>
-          <Flex
-            direction="column"
-            justify="space-between"
-            fontWeight="semibold"
-          >
-            <chakra.span>{wiki?.title}</chakra.span>
-            <Text fontSize="xs" fontWeight="light" my={2}>
-              {wiki && getWikiSummary(wiki, WikiSummarySize.Small)}
-            </Text>
+        <Flex
+          direction="column"
+          justify="space-between"
+          fontWeight="semibold"
+          p={4}
+        >
+          <chakra.span>{wiki?.title}</chakra.span>
+          <Text fontSize="xs" fontWeight="light" my={2}>
+            {wiki && getWikiSummary(wiki, CARD_DETAILS_LENGTH)}
+          </Text>
 
+          <Flex gap={3}>
             <NextLink href={`/account/${wiki?.user?.id}`} passHref>
-              <chakra.a color="blue">
-                {username || shortenAccount(wiki?.user?.id || '')}
-              </chakra.a>
+              <Box>
+                {avatar ? (
+                  <Avatar size="xs" src={avatar} />
+                ) : (
+                  <CustomAvatar
+                    size="20"
+                    variant="pixel"
+                    name="Unnamed"
+                    colors={AvatarColorArray}
+                  />
+                )}
+              </Box>
             </NextLink>
+            <Text fontSize="14px" color="linkColor">
+              <NextLink href={`/account/${wiki?.user?.id}`} passHref>
+                <Link href="passRef" color="brand.500" fontWeight="bold">
+                  {username || shortenAccount(wiki?.user?.id || '')}
+                </Link>
+              </NextLink>
+            </Text>
           </Flex>
         </Flex>
       </Flex>
