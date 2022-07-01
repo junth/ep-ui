32d697a87f7580deb7f77fe98560c1567ff123a6
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2022-07-01 21:23:38 +0530

    made editor to remove draft once the content gets published (#397)

--- src/utils/create-wiki.ts
+++ src/utils/create-wiki.ts
@@ -21,7 +21,10 @@ import { useAccount, useSignTypedData, useWaitForTransaction } from 'wagmi'
 import { NextRouter } from 'next/router'
 import { skipToken } from '@reduxjs/toolkit/dist/query'
 import { getWiki, useGetWikiQuery } from '@/services/wikis'
-import { getDraftFromLocalStorage } from '@/store/slices/wiki.slice'
+import {
+  getDraftFromLocalStorage,
+  removeDraftFromLocalStorage,
+} from '@/store/slices/wiki.slice'
 import { useToast } from '@chakra-ui/toast'
 import { store } from '@/store/store'
 import { Dict } from '@chakra-ui/utils'
@@ -235,6 +238,7 @@ export const useGetSignedHash = (deadline: number) => {
               setIsLoading(undefined)
               setActiveStep(3)
               setMsg(successMessage)
+              removeDraftFromLocalStorage()
               clearInterval(timer)
             }
           }
@@ -254,8 +258,8 @@ export const useGetSignedHash = (deadline: number) => {
           clearInterval(timer)
         }
       }, 3000)
-      // eslint-disable-next-line react-hooks/exhaustive-deps
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [refetch],
   )
 
